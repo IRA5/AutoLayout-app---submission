b48fcabd5e078a387204e4a7eb51e224c21b4e72
Ali <ira5@njit.edu>
2021-10-13 18:37:19 -0400

    Initial Commit

---
+++ AutoLayout app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout app
+//
+//  Created by Irtaza Ali on 10/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
